f9e8f072bd43129e1683624231926419a02c7d53
Szymon Gałecki <szymongalecki@gmail.com>
2022-05-13 18:54:02 +0200

    feat: alert button

--- src/App.js
+++ src/App.js
@@ -6,6 +6,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
+        <button onClick={() => alert("hello react")}>Click for alert</button>
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
